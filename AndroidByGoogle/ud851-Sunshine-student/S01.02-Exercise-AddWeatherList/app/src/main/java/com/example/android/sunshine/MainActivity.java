1ffa983b78886fc8b1fd3620109a02227270baf7
Dushyant <6dushyantmishra20101@gmail.com>
2017-06-16 16:05:06 +0530

    Added Android Course by Google Solution

---
+++ AndroidByGoogle/ud851-Sunshine-student/S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.sunshine;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.test.suitebuilder.TestMethod;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // COMPLETD (1) Create a field to store the weather display TextView
+    TextView displayWeatherList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forecast);
+
+        // TCOMPLETD (2) Use findViewById to get a reference to the weather display TextView
+        displayWeatherList = (TextView) findViewById(R.id.tv_weather_data);
+
+        // COMPLETED(3) Create an array of Strings that contain fake weather data
+        String [] weatherLists = {"Today", "Tomorrow", "Yesterday"};
+
+        // COMPLETED (4) Append each String from the fake weather data array to the TextView
+        for(String weather : weatherLists){
+            displayWeatherList.append(weather + "\n\n\n");
+        }
+    }
+}
---
+++ AndroidByGoogle/ud851-Sunshine-student/S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.sunshine;
+
+import android.graphics.Region;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.android.sunshine.data.SunshinePreferences;
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mWeatherTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forecast);
+
+        /*
+         * Using findViewById, we get a reference to our TextView from xml. This allows us to
+         * do things like set the text of the TextView.
+         */
+        mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
+
+        // COMPLETED (4) Delete the dummy weather data. You will be getting REAL data from the Internet in this lesson.
+        /*
+         * This String array contains dummy weather data. Later in the course, we're going to get
+         * real weather data. For now, we want to get something on the screen as quickly as
+         * possible, so we'll display this dummy data.
+         */
+
+
+        // COMPLETED (3) Delete the for loop that populates the TextView with dummy data
+        /*
+         * Iterate through the array and append the Strings to the TextView. The reason why we add
+         * the "\n\n\n" after the String is to give visual separation between each String in the
+         * TextView. Later, we'll learn about a better way to display lists of data.
+         */
+
+
+        // COMPLETED (9) Call loadWeatherData to perform the network request to get the weather
+        loadWeatherData();
+    }
+
+    // TODO (8) Create a method that will get the user's preferred location and execute your new AsyncTask and call it loadWeatherData
+    private void loadWeatherData(){
+        String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        new getWeatherData().execute(location);
+    }
+    // COMPLETED (5) Create a class that extends AsyncTask to perform network requests
+    // COMPLETED (6) Override the doInBackground method to perform your network requests
+    // COMPLETED (7) Override the onPostExecute method to display the results of the network request
+    public class getWeatherData extends AsyncTask<String, Void, String[]>{
+//        @Override
+//        protected void onPreExecute(){
+//            progressBar.setVisibility(View.VISIBLE);
+//        }
+
+        @Override
+        protected String[] doInBackground(String... params) {
+            if(params.length == 0){
+                return null;
+            }
+            String dataUrl = params[0];
+            String weatherResults = null;
+            try {
+                weatherResults = NetworkUtils.getResponseFromHttpUrl(NetworkUtils.buildUrl(dataUrl));
+                String [] simpleJSONWeatherData = OpenWeatherJsonUtils.getSimpleWeatherStringsFromJson(MainActivity.this, weatherResults);
+                return simpleJSONWeatherData;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String[] weatherResults) {
+            // COMPLETED (27) As soon as the loading is complete, hide the loading indicator
+            //progressBar.setVisibility(View.INVISIBLE);
+            if(weatherResults!=null){
+                for(String weatherString: weatherResults){
+                    mWeatherTextView.append(weatherString + "\n\n\n");
+                }
+            }
+            // COMPLETED (16) Call showErrorMessage if the result is null in onPostExecute
+        }
+    }
+}
